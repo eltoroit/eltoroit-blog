60d985910dfdd72c73cee2a766baa9ecb7a488ce
aperez <aperez@salesforce.com>
2016-02-11 07:16:15 -0500

    Fixed Bug

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
-var env = require('node-env-file');
+console.log('#ElToroIT: === === === SERVER RESTART === === === [' + new Date() + "]");
+
+var express = require('express');				// http://expressjs.com/en
+var http = require('http');						// https://nodejs.org/api/http.html
+var https = require('https');					// https://nodejs.org/api/https.html
+var fs = require('fs');							// https://nodejs.org/api/fs.html
+var env = require('node-env-file');				// https://github.com/grimen/node-env-file
+const queryString = require('query-string');	// https://www.npmjs.com/package/query-string
 
 var app = express();
 var port = process.env.PORT || 5000;
@@ -22,11 +25,13 @@ app.get('/test', function(request, response) {
 	response.send(result);
 });
 app.get('/', function(reqHTTP, resHTTP) {
+	console.log("#ElToroIT: Root Called');
 	sfdcLoginOauthUNPW(function(sfdcLoginOutput) {
 		resHTTP.render('LCOut', {sfdcLoginOutput: sfdcLoginOutput});
 	});
 });
 app.get('/Blog.app', function(reqHTTP, resHTTP) {
+	console.log("#ElToroIT: Blog.app Called');
 	sfdcLoginOauthUNPW(function(sfdcLoginOutput) {
 		resHTTP.render('LCOut', {sfdcLoginOutput: sfdcLoginOutput});
 	});
@@ -60,7 +65,7 @@ function sfdcLoginOauthUNPW(callback) {
 		password: process.env.password,
 		format: "json",
 	};
-	console.log('postData', postData);
+	console.log('#ElToroIT-REMOVE COMMENT: postData', postData);
 	postData = queryString.stringify(postData);
 	
 	var options = {
@@ -74,20 +79,22 @@ function sfdcLoginOauthUNPW(callback) {
 			'Content-Length': postData.length,
 		}
 	};
-	console.log('options', options);
+	console.log('#ElToroIT-REMOVE COMMENT: options', options);
 	
 	var reqWS = https.request(options, function(resWS) {
 		resWS.setEncoding('utf8');
 		resWS.on('data', function(chunk) {
+			console.log('#ElToroIT-REMOVE COMMENT: Received chunk');
 			sfdcLoginOutput = JSON.parse(chunk);
 		});
 		resWS.on('end', function() {
-			console.log('sfdcLoginOutput', sfdcLoginOutput);
+			console.log('#ElToroIT-REMOVE COMMENT: Completed');
+			console.log('#ElToroIT-FIXED COMMENT: sfdcLoginOutput', sfdcLoginOutput);
 			callback(sfdcLoginOutput);
 		})
 	});
 	reqWS.on('error', function(e) {
-		console.log('problem with request: ' + e.message);
+		console.log('#ElToroIT-ERROR: problem with request: ' + e.message);
 	});
 
 	// write data to request body

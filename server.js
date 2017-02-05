6a6ba53049ccbd0715dbb9e1bc895e4b8470c896
aperez <aperez@salesforce.com>
2017-02-06 04:10:11 +0530

    Trying to fix "has been blocked by CORS policy: Request requires preflight, which is disallowed to follow cross-origin redirect."

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ console.log('#ElToroIT: HTTP Port: ' + port);
 console.log('#ElToroIT: HTTPS Port: ' + https_port);
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+	express.static(__dirname + '/public')
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 // Test page.
 var times = null;
